ec077270258f4aec0bb49cdc4047ef338617e3f0
Wilson <wilson.souza@abril.com.br>
2013-08-02 11:21:21 -0300

    [US23528] simplify version

--- lib/heuristic_cache/version.rb
+++ lib/heuristic_cache/version.rb
@@ -1,24 +1,6 @@
 # encoding: UTF-8
 module AlxHeuristicCache
 
-  module VERSION
-    @@version = nil
-    def self.version
-      if @@version == nil
-        txt = File.expand_path(File.join(*%w[.. .. .. CURRENT_VERSION]), __FILE__)
-        if File.exists?(txt)
-          @@version = File.read(txt).chomp.gsub("v","")
-        else
-          @@version = "0.0.1"
-        end
-      end
-      @@version
-    end
-
-    def self.to_s
-      self.version
-    end
-
-  end
+  VERSION = '0.0.1'
 
 end
